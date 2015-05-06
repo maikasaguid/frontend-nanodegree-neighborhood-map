e28a6558959bfad05f51646c308fffc1f65ad8bb
maikasaguid <maikasaguid@users.noreply.github.com>
2015-05-06 13:50:35 -1000

    Filter working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -48,83 +48,56 @@ google.maps.event.addListener(map, 'zoom_changed', function() {
   if(map.getZoom() < minZoomLevel) map.setZoom(minZoomLevel);
 });
 
-function convertToMarkers(places) {
-  var i = 0,
-  marks = [];
-
-  while(i < places.length) {
-    marks[i] = new google.maps.Marker({
-      position: new google.maps.LatLng(places[i].lat, places[i].lng),
-      title: places[i].name,
-      map: map
-    });
-
-    i++;
-  }
-
-  return marks;
-}
-
 var ourTopPizza = [
   {
     name: 'Big Kahuna\'s Pizza',
     lat: 21.335522,
-    lng: -157.91654500000004,
-    show: true
+    lng: -157.91654500000004
   },
   {
-    name: 'J.J. Dolans',
+    name: 'J.J. Dolan\'s',
     lat: 21.310943,
-    lng: -157.860536,
-    show: true
+    lng: -157.860536
   },
   {
     name: 'Bravo Restaurant',
     lat: 21.383593,
-    lng: -157.94557099999997,
-    show: true
+    lng: -157.94557099999997
   },
   {
     name: 'Rosarina Pizza',
     lat: 21.31229,
-    lng: -157.862618,
-    show: true
+    lng: -157.862618
   },
   {
     name: 'Doughlicious',
     lat: 21.294302,
-    lng: -157.841133,
-    show: true
+    lng: -157.841133
   },
   {
     name: 'La Pizza Rina',
     lat: 21.298095,
-    lng: -157.83961599999998,
-    show: true
+    lng: -157.83961599999998
   },
   {
     name: 'Brick Oven Pizza',
     lat: 21.332582,
-    lng: -158.082038,
-    show: true
+    lng: -158.082038
   },
   {
     name: 'Pizza Corner',
     lat: 21.342423,
-    lng: -158.124125,
-    show: true
+    lng: -158.124125
   },
   {
     name: 'Serino\'s Pizza',
     lat: 21.308459,
-    lng: -157.810562,
-    show: true
+    lng: -157.810562
   },
   {
     name: 'Boston\'s North End Pizza Restaurant',
     lat: 21.380137,
-    lng: -157.93820600000004,
-    show: true
+    lng: -157.93820600000004
   }
 ];
 
@@ -132,108 +105,120 @@ var yelpTopPizza = [
   {
     name: 'Hiking Hawaii Cafe',
     lat: 21.285458,
-    lng: -157.835197,
-    show: true
+    lng: -157.835197
   },
   {
     name: 'Kaneohe\'s Boston Pizza',
     lat: 21.418950,
-    lng: -157.804038,
-    show: true
+    lng: -157.804038
   },
   {
     name: 'Amina Pizzeria',
     lat: 21.292439,
-    lng: -157.836421,
-    show: true
+    lng: -157.836421
   },
   {
     name: 'JJ Dolan\'s',
     lat: 21.310943,
-    lng: -157.860536,
-    show: true
+    lng: -157.860536
   },
   {
     name: 'The Lovin\' Oven',
     lat: 21.276885,
-    lng: -157.823936,
-    show: true
+    lng: -157.823936
   },
   {
     name: 'Uncle Bo\'s Pupu Bar & Grill',
     lat: 21.277740,
-    lng: -157.813856,
-    show: true
+    lng: -157.813856
   },
   {
     name: 'Marketplace Cafe',
     lat: 21.292547,
-    lng: -157.841960,
-    show: true
+    lng: -157.841960
   },
   {
     name: 'Impossibles Pizza',
     lat: 21.664547,
-    lng: -158.050516,
-    show: true
+    lng: -158.050516
   },
   {
     name: 'Arancino on Beachwalk',
     lat: 21.280135,
-    lng: -157.830890,
-    show: true
+    lng: -157.830890
   },
   {
     name: 'Fendu Boulangrie',
     lat: 21.307802,
-    lng: -157.810460,
-    show: true
+    lng: -157.810460
   }
 ];
+
+function Location(name, lat, lng, show) {
+  this.name = name;
+  this.lat = lat;
+  this.lng = lng;
+  this.marker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat, lng),
+    title: name,
+    map: map
+  });
+  this.show = ko.observable(show);
+}
+
+function convertToLocations(places) {
+  var locs = [];
+  
+  for(var i=0; i < places.length; i++) {
+    locs[i] = new Location(places[i].name, places[i].lat, places[i].lng, true);
+  }
+
+  return locs;
+}
 
 function AppViewModel() {
   var self = this;
 
-  self.markers = ko.observableArray(convertToMarkers(ourTopPizza));
-  self.places = ko.observableArray(ourTopPizza);
   self.filter = ko.observable('');
   self.currentList = ourTopPizza;
+  self.currentListString = 'our';
+  self.locations = ko.observableArray(convertToLocations(self.currentList));
 
   self.choosePizzaList = function(data, event) {
-    ko.utils.arrayForEach(self.markers(), function(marker) {
-      marker.setMap(null);
-    });
+    if(event.target.id != self.currentListString) {
+      self.currentListString = event.target.id;
 
-    self.places([]);
+      ko.utils.arrayForEach(self.locations(), function(location) {
+        location.marker.setMap(null);
+      });
 
-    if(event.target.id === 'yelp') {
-      self.currentList = yelpTopPizza;
-    }
-    if(event.target.id === 'our') {
-      self.currentList = ourTopPizza;
-    }
-    if(event.target.id === 'all') {
-      
-    }
+      self.locations([]);
 
-    self.markers(convertToMarkers(self.currentList));
-    self.places(self.currentList);
+      if(event.target.id === 'yelp') {
+        self.currentList = yelpTopPizza;
+      }
+      if(event.target.id === 'our') {
+        self.currentList = ourTopPizza;
+      }
+      if(event.target.id === 'all') {
+        
+      }
+
+      self.locations(convertToLocations(self.currentList));
+      self.filterListMap();
+    }
   };
 
-  self.filteredPlaces = ko.computed(function() {
-    if(!self.filter()) {
-      return self.places();
-    }
-    else {
-      return ko.utils.arrayFilter(self.places(), function(place) { 
-        return place.name.toLowerCase().indexOf(self.filter().toLowerCase()) !== -1;
-      });
-    }
-  }, self);
-
   self.filterListMap = function(data, event) {
-    self.markers(convertToMarkers(self.filteredPlaces()));
-    self.places(self.filteredPlaces());
+    ko.utils.arrayForEach(self.locations(), function(location) {
+      location.show(true);
+      location.marker.setMap(map);
+
+      if(location.name.toLowerCase().indexOf(self.filter().toLowerCase()) == -1) {
+        location.show(false);
+        location.marker.setMap(null);
+      }
+    });
   }
 }
 
